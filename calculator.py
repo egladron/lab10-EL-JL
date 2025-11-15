f6464edd3e5643abe4f28228c61115415cd31b47
Joshua Lovett <joshualovett@Joshuas-MBP.cable.tks-net.com>
2025-11-15 02:59:23 +0100

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,6 +1,5 @@
 import math
 
-<<<<<<< HEAD
 def add(a, b): 
     return a + b
 def sub(a, b):
@@ -16,36 +15,4 @@ def log(a,b):
         raise ValueError("Invalid input for logarithm")
     return math.log(b, a)
 def exp(a, b):
-    return a ** b
-
-=======
-One function per operation, in order.
-"""
-# https://github.com/egladron/lab10-EL-JL
-# Partner 1: Edwin Ladron de Guevara
-# Partner 2: Joshua Lovett
-
-import math
-def add(a, b): 
-    return a + b
->>>>>>> f56ffc995ba1dffe749cb8245524bf8dce7326af
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    return b / a
-
-def logarithm(a, b):
-    try:
-        return math.log(b, a)
-    except ValueError:
-        raise ValueError
-
-def exponent(a, b):
     return a ** b
